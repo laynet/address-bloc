194e88255a19df99d2fb8b0d84feb95d0654c59c
laynet <layneingramtaylor@gmail.com>
2019-05-09 16:58:40 -0500

    specific spec test failures aren't matching checkpont failures

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -1,6 +1,29 @@
 const ContactController = require("../controllers/ContactController");
+const sequelize = require("../db/models/index".sequelize);
 
 describe("ContactController", () => {
+  beforeEach(done => {
+    this.book = new ContactController();
+
+    //#1
+    sequelize
+      .sync({ force: true })
+      .then(res => {
+        done();
+      })
+      .catch(err => {
+        done();
+      });
+  });
+
+  //#2
+  describe("#addContact()", () => {
+    it("should add a single contact into the book", () => {
+      expect(this.book.contact.length).toBe(0);
+      this.book.addContact("Alice", "001-101-1010");
+      expect(this.book.contact.length).toBe(1);
+    });
+  });
   it("should be defined", () => {
     expect(ContactController).toBeDefined();
   });
