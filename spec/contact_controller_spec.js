f945a6bdfeb12c955118ea37ef89a141bae46b36
Layne <layneingramtaylor@gmail.com>
2019-05-11 22:11:47 -0500

    test failures aren't matching again, there's 3 instead of 2

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -1,5 +1,5 @@
 const ContactController = require("../controllers/ContactController");
-const sequelize = require("../db/models/index".sequelize);
+const sequelize = require("../db/models/index").sequelize;
 
 describe("ContactController", () => {
   beforeEach(done => {
