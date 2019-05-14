9bb0cfe263fe0dfe21a6f6e9570b420f933ba5f9
laynet <layneingramtaylor@gmail.com>
2019-05-14 15:13:08 -0500

    test failures aren't matching - cannot read length of undefined

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -1,5 +1,11 @@
 const inquirer = require("inquirer");
 
 module.exports = class ContactController {
-  constructor() {}
+  constructor() {
+    this.contacts = [];
+  }
+
+  addContact(name, phone) {
+    this.contacts.push({ name, phone });
+  }
 };
