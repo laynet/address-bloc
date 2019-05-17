37d976ef45da9c46ecd84b8d6f9ff3e917b8121d
Layne <layneingramtaylor@gmail.com>
2019-05-16 23:47:07 -0500

    test failures don't match checkpoint examples

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -37,3 +37,33 @@ describe("ContactController", () => {
     expect(ContactController).toBeDefined();
   });
 });
+
+describe("#getContacts()", () => {
+  it("should return an epty array when no contacts are available", done => {
+    this.book
+      .getContacts()
+      .then(contacts => {
+        expect(contacts.length).toBe(0);
+        done();
+      })
+      .catch(err => {
+        console.log(err);
+        done();
+      });
+  });
+
+  it("should return an empty array of contacts when contacts are available", done => {
+    this.book
+      .addContact("Alice", "001-101-1010", "alice@example.com")
+      .then(() => {
+        this.book.getContacts().then(contacts => {
+          expect(contacts.length).toBe(1);
+          done();
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        done();
+      });
+  });
+});
