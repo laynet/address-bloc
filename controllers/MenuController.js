3c2593094ed1331ec217dd880ae9057f12bc8862
Layne <layneingramtaylor@gmail.com>
2019-05-18 16:39:53 -0500

    checkpoint tests not passing

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -10,7 +10,13 @@ module.exports = class MenuController {
         type: "list",
         name: "mainMenuChoice",
         message: "Please choose from an option below: ",
-        choices: ["Add new contact", "Get Date", "Exit"]
+        choices: [
+          "Add new contact",
+          "View all contacts",
+          "Search for a contact",
+          "Get Date",
+          "Exit"
+        ]
       }
     ];
     this.book = new ContactController();
@@ -24,6 +30,12 @@ module.exports = class MenuController {
         switch (response.mainMenuChoice) {
           case "Add new contact":
             this.addContact();
+            break;
+          case "View all contacts":
+            this.getContacts();
+            break;
+          case "Search for a contact":
+            this.search();
             break;
           case "Get Date":
             this.getDate();
@@ -61,6 +73,26 @@ module.exports = class MenuController {
     });
   }
 
+  getContacts() {
+    this.clear();
+    this.book
+      .getContacts()
+      .then(contacts => {
+        for (let contact of contacts) {
+          console.log(`
+        name: ${contact.name}
+        phone number: ${contact.phone}
+        email: ${contact.email}
+        ______________________`);
+        }
+        this.main();
+      })
+      .catch(err => {
+        console.log(err);
+        this.main();
+      });
+  }
+
   getDate() {
     console.log(day);
     this.main();
@@ -77,5 +109,73 @@ module.exports = class MenuController {
 
   remindMe() {
     return "Learning is a life-long pursuit";
+  }
+
+  search() {
+    inquirer
+      .prompt(this.book.searchQuestions)
+      .then(target => {
+        this.book.search(target.name).then(contact => {
+          if (contact === null) {
+            this.clear();
+            console.log("contact not found");
+            this.search();
+          } else {
+            this.showContact(contact);
+          }
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        this.main();
+      });
+  }
+  showContact(contact) {
+    this._printContact(contact);
+    inquirer
+      .prompt(this.book.showContactQuestions)
+      .then(answer => {
+        switch (answer.selected) {
+          case "Delete contact":
+            this.delete(contact);
+            break;
+          case "Main menu":
+            this.main();
+            break;
+          default:
+            console.log("Something went wrong.");
+            this.showContact(contact);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        this.showContact(contact);
+      });
+  }
+
+  _printContact(contact) {
+    console.log(`
+      name: ${contact.name}
+      phone number: ${contact.phone}
+      email: ${contact.email}
+      ---------------`);
+  }
+  delete(contact) {
+    inquirer
+      .prompt(this.book.deleteConfirmQuestions)
+      .then(answer => {
+        if (answer.confirmation) {
+          this.book.delete(contact.id);
+          console.log("contact deleted!");
+          this.main();
+        } else {
+          console.log("contact not deleted");
+          this.showContact(contact);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        this.main();
+      });
   }
 };
