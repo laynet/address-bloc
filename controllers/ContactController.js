e0757d1bf5199ee576f1ecb347127d9a754b6353
Layne <layneingramtaylor@gmail.com>
2019-05-20 21:34:31 -0500

    still getting failures

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -45,4 +45,26 @@ module.exports = class ContactController {
     }
     return null;
   }
+
+  binarySearch(contacts, target) {
+    let min = 0;
+    let max = contacts.length - 1;
+    let mid;
+
+    while (min <= max) {
+      // #1
+      mid = Math.floor((min + max) / 2);
+      let currentContact = contacts[mid];
+      //#2
+      if (currentContact.name > target) {
+        max = mid - 1;
+      } else if (currentContact.name < target) {
+        min = mid - 1;
+      } else {
+        return contacts[mid];
+      }
+    }
+
+    return null;
+  }
 };
